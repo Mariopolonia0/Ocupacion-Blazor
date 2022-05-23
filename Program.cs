2033664a93363e3ab0467ce993eab48cde31d671
Mario <mariopolonia0@gmail.com>
2022-05-23 18:31:04 -0400

    validacion lista

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Blazored.Toast;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddBlazoredToast();
 
 var app = builder.Build();
 
@@ -16,7 +18,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+//Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Your License Key");
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
